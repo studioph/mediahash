53198bc12ff21376d9528078480f6250771d5ba2
Paul Hutchings <dev@studiop.page>
2024-05-23 18:56:13 -0400

    Add image support

---
+++ src/mediahash/image.py
@@ -0,0 +1,69 @@
+from dataclasses import dataclass
+import hashlib
+import logging
+from typing import IO
+
+import imagehash
+import numpy as np
+try:
+    import pillow_avif  # pylint: disable=unused-import
+except ImportError:
+    pass
+from imagehash import ImageHash
+from PIL import Image, UnidentifiedImageError
+
+from .util import reraise
+
+from .types import MediaError, MediaFingerprint
+
+LOG = logging.getLogger(__name__)
+
+extensions = (".jpg", ".jpeg", ".png", ".webp", ".avif")
+
+rethrow = reraise(UnidentifiedImageError, as_=MediaError)
+
+@dataclass(frozen=True, kw_only=True)
+class ImageInfo:
+    width: int
+    height: int
+    size: int
+
+class ImageFingerprint:
+    def __init__(self, ihash: ImageHash):
+        self._fp = ihash
+
+    def __sub__(self, other: ImageHash) -> int:
+        return self._fp - other
+
+    def __bytes__(self) -> bytes:
+        return self._fp.hash.tobytes()
+
+@staticmethod
+@rethrow
+def analyze(media: IO) -> ImageInfo:
+    LOG.debug("Analyzing %s", media.name)
+    with Image.open(media) as img:
+        return ImageInfo(size=len(img.tobytes()), width=img.width, height=img.height)
+
+@staticmethod
+@rethrow
+def checksum(media: IO) -> bytes:
+    LOG.debug("Calculating hash for %s", media.name)
+    with Image.open(media) as img:
+        hash_ = hashlib.md5(img.tobytes())
+    return hash_.digest()
+
+mhash = checksum
+
+@staticmethod
+@rethrow
+def fingerprint(media: IO) -> MediaFingerprint:
+    LOG.debug("Fingerprinting %s", media.name)
+    with Image.open(media) as img:
+        fingerprint_ = imagehash.phash(img)
+    return ImageFingerprint(fingerprint_)
+
+@staticmethod
+def parse_fingerprint(__bytes: bytes) -> MediaFingerprint:
+    arr = np.frombuffer(__bytes, dtype=np.uint8)
+    return ImageFingerprint(ImageHash(arr))
