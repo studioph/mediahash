0eb9374d4b336d638671a4cd64e09287e429336e
Paul Hutchings <dev@studiop.page>
2024-05-23 18:56:06 -0400

    Add base types

---
+++ src/mediahash/types.py
@@ -0,0 +1,42 @@
+from abc import abstractmethod
+from collections.abc import Collection
+from typing import IO, Protocol
+
+
+class MediaError(Exception):
+    """Aggregate error for different media types"""
+
+
+
+class MediaFingerprint(Protocol):
+    @abstractmethod
+    def __sub__(self, other) -> int | float:
+        ...
+
+    @abstractmethod
+    def __bytes__(self) -> bytes:
+        ...
+
+
+class MediaImpl(Protocol):
+    extensions: Collection[str]
+
+    @abstractmethod
+    def analyze(self, media: IO):
+        ...
+
+    @abstractmethod
+    def checksum(self, media: IO) -> bytes:
+        ...
+
+    @abstractmethod
+    def mhash(self, media: IO) -> bytes:
+        ...
+
+    @abstractmethod
+    def fingerprint(self, media: IO) -> MediaFingerprint:
+        ...
+
+    @abstractmethod
+    def parse_fingerprint(self, __bytes: bytes) -> MediaFingerprint:
+        ...
